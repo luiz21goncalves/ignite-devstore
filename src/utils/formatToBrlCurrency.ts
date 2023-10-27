e8e6fd7c38eecd2267dbde9aa4e5a198f8a1185c
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-10-27 07:58:05 -0300

    chore: add function to format currency

---
+++ src/utils/formatToBrlCurrency.ts
@@ -0,0 +1,8 @@
+export function formatToBrlCurrency(price: number) {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(price)
+}
