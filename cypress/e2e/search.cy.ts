18b86cba3245e9fc5d2061deaaf0b025f80a0766
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-11-11 09:49:14 -0300

    chore: ignore uncaught exceptions for search test

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -9,6 +9,10 @@ describe('search products', () => {
   })
 
   it('should not be able to visit search page without a search query', () => {
+    cy.on('uncaught:exception', () => {
+      return false
+    })
+
     cy.visit('/search')
 
     cy.location('pathname').should('equal', '/')
